07a9295b694ee8f2b2f2ee8d4e0603d451d14c40
infodp <diprimiogustavo@gmail.com>
2024-08-07 18:58:03 -0300

    add search

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Card from "./Card";
+import { useEffect } from "react";
 import { fetchData } from "../assets/getMovies";
 
 function Show() {
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const getMovies = async () => {
@@ -13,7 +15,7 @@ function Show() {
         const moviesData = edges.map((edge) => {
           const entity = edge.node.entity;
           return {
-            title: entity.titleText.text,
+            title: entity.titleText?.text || "Untitled",
             imageUrl: entity.primaryImage?.url,
             year: entity.releaseDate?.year,
             imdbLink: `https://www.imdb.com/title/${entity.id}`,
@@ -27,19 +29,42 @@ function Show() {
     getMovies();
   }, []);
 
+  const handleSearchChange = (event) => {
+    //console.log(event.target.value);
+    setSearch(event.target.value);
+  };
+
+  const filteredMovies = movies.filter(
+    (movie) =>
+      movie.title.toLowerCase().includes(search.toLowerCase()) ||
+      (movie.year && movie.year.toString().includes(search))
+  );
+
   return (
     <div>
       <h1>Movies</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search Movies..."
+          value={search}
+          onChange={handleSearchChange}
+        />
+      </div>
       <div className="movie-list">
-        {movies.map((movie, index) => (
-          <Card
-            key={index}
-            title={movie.title}
-            imageUrl={movie.imageUrl}
-            year={movie.year}
-            imdbLink={movie.imdbLink}
-          />
-        ))}
+        {filteredMovies.length > 0 ? (
+          filteredMovies.map((movie, i) => (
+            <Card
+              key={i}
+              title={movie.title}
+              imageUrl={movie.imageUrl}
+              year={movie.year}
+              imdbLink={movie.imdbLink}
+            />
+          ))
+        ) : (
+          <p>No movies found</p>
+        )}
       </div>
     </div>
   );
