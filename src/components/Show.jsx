94f574588180a1be2366f4fa1422c06983fc8cb0
infodp <diprimiogustavo@gmail.com>
2024-08-09 10:00:47 -0300

    add loading spinner

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import Card from "./Card";
 import { useEffect } from "react";
 import { fetchData } from "../assets/getMovies";
+import { Puff, RotatingLines, Vortex } from "react-loader-spinner";
 
 function Show() {
   const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getMovies = async () => {
@@ -24,6 +26,8 @@ function Show() {
         setMovies(moviesData);
       } catch (error) {
         console.error("Error fetching data: ", error);
+      } finally {
+        setLoading(false);
       }
     };
     getMovies();
@@ -52,7 +56,14 @@ function Show() {
         />
       </div>
       <div className="movie-list">
-        {filteredMovies.length > 0 ? (
+        {loading ? (
+          <Vortex 
+            height={120}
+            width={120}
+            color="#12c2e9"
+            ariaLabel="puff-loading"
+          />
+        ) : filteredMovies.length > 0 ? (
           filteredMovies.map((movie, i) => (
             <Card
               key={i}
